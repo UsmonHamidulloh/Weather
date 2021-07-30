892d990024bcca0ce7d8a880198f0db6ce1580ba
UsmonHamidulloh <usman.x.0221@gmail.com>
2021-07-30 15:17:13 +0500

    added resources [for UI]

--- app/src/main/java/com/hamidulloh/weather/MainActivity.kt
+++ app/src/main/java/com/hamidulloh/weather/MainActivity.kt
@@ -11,26 +11,9 @@ import com.hamidulloh.weather.viewmodelfactory.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var vModel: MainViewModel
-    private lateinit var vMFactory: MainViewModelFactory
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val retrofitInstance = RetrofitInstance
-
-        binding.requestBtn.setOnClickListener {
-            vMFactory = MainViewModelFactory(retrofitInstance, binding.countryInput.text.toString())
-            vModel = ViewModelProvider(this, vMFactory).get(MainViewModel::class.java)
-
-            vModel.getData.observe(this, {
-                binding.apply {
-                    countryNameTxt.text = "Country: ${it.name}"
-                    descTxt.text = "Description: ${it.weather[0].desc}"
-                    tempTxt.text = "Temperature: ${it.main.temp}"
-                }
-            })
-        }
     }
 }
