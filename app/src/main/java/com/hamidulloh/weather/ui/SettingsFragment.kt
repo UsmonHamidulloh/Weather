a4c617d2c7267e0b08504eca085e0a70d7d1d6de
UsmonHamidulloh <usman.x.0221@gmail.com>
2021-08-07 16:03:26 +0500

    SettingsFragment.kt

--- app/src/main/java/com/hamidulloh/weather/ui/SettingsFragment.kt
+++ app/src/main/java/com/hamidulloh/weather/ui/SettingsFragment.kt
@@ -1,10 +1,14 @@
 package com.hamidulloh.weather.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hamidulloh.weather.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -18,5 +22,49 @@ class SettingsFragment : Fragment() {
     ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.backLl.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.rateLl.setOnClickListener {
+            Toast.makeText(
+                requireContext(),
+                "Coming soon", Toast.LENGTH_LONG
+            ).show()
+        }
+        binding.aboutLl.setOnClickListener {
+            Toast.makeText(
+                requireContext(),
+                "Coming soon", Toast.LENGTH_LONG
+            ).show()
+        }
+        binding.theTeamTxt.setOnClickListener {
+            Toast.makeText(
+                requireContext(), "Coming soon", Toast.LENGTH_LONG
+            ).show()
+        }
+        binding.darkModeLl.setOnClickListener {
+            Toast.makeText(
+                requireContext(), "Coming soon", Toast.LENGTH_LONG
+            ).show()
+        }
+
+        binding.reportIssueLl.setOnClickListener {
+            val reportIssue = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://t.me/usmonga_murojaat_bot")
+            )
+            startActivity(reportIssue)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
