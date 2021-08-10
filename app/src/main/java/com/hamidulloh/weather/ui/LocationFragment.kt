829570f10ba7cc11e4f736738612eac68a6c20a7
UsmonHamidulloh <usman.x.0221@gmail.com>
2021-08-10 14:33:28 +0500

    fragment_location.xml design is ready

--- app/src/main/java/com/hamidulloh/weather/ui/LocationFragment.kt
+++ app/src/main/java/com/hamidulloh/weather/ui/LocationFragment.kt
@@ -1,6 +1,31 @@
 package com.hamidulloh.weather.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.hamidulloh.weather.databinding.FragmentLocationBinding
 
 class LocationFragment : Fragment() {
+    private var _binding: FragmentLocationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLocationBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.backLl.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
