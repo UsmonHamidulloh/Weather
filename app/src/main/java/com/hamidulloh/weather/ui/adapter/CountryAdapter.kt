556395b3e3bb738ef541a2f352cedebc484ed096
UsmonHamidulloh <usman.x.0221@gmail.com>
2021-08-16 15:16:13 +0500

    adapter is working ⚙

--- app/src/main/java/com/hamidulloh/weather/ui/adapter/CountryAdapter.kt
+++ app/src/main/java/com/hamidulloh/weather/ui/adapter/CountryAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hamidulloh.weather.databinding.ItemCountryBinding
 import com.hamidulloh.weather.model.Country
 import com.hamidulloh.weather.utils.setWeatherIcon
+import timber.log.Timber
 
 class CountryAdapter : ListAdapter<Country, CountryAdapter.ViewHolder>(CountryDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,12 +28,13 @@ class CountryAdapter : ListAdapter<Country, CountryAdapter.ViewHolder>(CountryDi
         holder.binding.name.text = country.name
         holder.binding.temp.text = "${country.temp}°C"
         holder.binding.description.text = country.desc
+        Timber.tag("TAG").d(country.weather)
         holder.binding.weather.setWeatherIcon(country.weather)
     }
 
     class CountryDiffCallback : DiffUtil.ItemCallback<Country>() {
         override fun areItemsTheSame(oldItem: Country, newItem: Country): Boolean =
-            oldItem.temp == newItem.temp
+            oldItem.name == newItem.name
 
         override fun areContentsTheSame(oldItem: Country, newItem: Country): Boolean =
             oldItem == newItem
--- app/src/main/java/com/hamidulloh/weather/ui/fragments/LocationFragment.kt
+++ app/src/main/java/com/hamidulloh/weather/ui/fragments/LocationFragment.kt
@@ -6,12 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hamidulloh.weather.databinding.FragmentLocationBinding
+import com.hamidulloh.weather.model.Country
+import com.hamidulloh.weather.model.Regions
+import com.hamidulloh.weather.ui.adapter.CountryAdapter
 
 class LocationFragment : Fragment() {
     private var _binding: FragmentLocationBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var cAdapter: CountryAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +31,27 @@ class LocationFragment : Fragment() {
 
         binding.backLl.setOnClickListener {
             findNavController().popBackStack()
+        }
+
+        val regions = ArrayList<Country>()
+
+        Regions.values().sortedArray().forEach {
+            regions.add(
+                Country(
+                    name = getString(it.stringResLoc),
+                    temp = 22,
+                    desc = "clear sky",
+                    weather = "1"
+                )
+            )
+        }
+
+        cAdapter = CountryAdapter()
+        cAdapter.submitList(regions)
+
+        binding.recyclerView.apply {
+            adapter = cAdapter
+            layoutManager = LinearLayoutManager(context)
         }
     }
 }
